915a8036befb75683e147b2db72e9b9dd01c1937
Edmond Dyogi III <edmond.dyogi@hkcolo.com>
2019-03-26 00:07:05 +0800

    Modified layout to add apache access log

--- .tmux/config/default-layout.sh
+++ .tmux/config/default-layout.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
+tmux split-window -v -p 25 -t 0 "lnav /var/log/apache2/error.log"
+tmux split-window -h -t 1 "lnav /var/log/apache2/access.log"
 tmux split-window -h -p 20 -t 0 "htop"
 tmux split-window -v -t 1 "sudo su"
-tmux split-window -v -t 2
-tmux split-window -v -p 25 -t 0 "lnav /var/log/apache2/error.log"
 tmux select-pane -t 0
 tmux send-keys -t 0 "powerline-daemon -q" enter
 tmux send-keys -t 0 "powerline-config tmux setup" enter
